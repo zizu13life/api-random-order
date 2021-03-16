b7e0eedef36bac8c788b9a6e8f92e4660f49ea13
Nicolas Haiduchok <nico13051995@gmail.com>
2021-03-16 03:37:21 +0200

    + add log to config

--- src/modules/config/conts.ts
+++ src/modules/config/conts.ts
@@ -3,3 +3,5 @@ export const WEB_HOST = process.env.API_RANDOM_ORDER_WEB_HOST || 'https://localh
 export const API_INTERNAL_PORT = Number.parseInt(process.env.PORT) || 3000;
 
 export const JWT_SECRET_KEY = process.env.API_RANDOM_ORDER_JWT_SECRET_KEY || 'JWT_SECRET_KEY';
+
+console.log(`${API_HOST}, ${WEB_HOST}, ${API_INTERNAL_PORT}`);
